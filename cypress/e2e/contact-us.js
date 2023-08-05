5edd919b54cafbb719bca6ff22b0d7f6941c2abd
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-08-05 15:27:53 +0200

    add first cypress test

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,12 @@
+describe("Test Contact Us form on WebDriverUni", () => {
+    it("Should be able to make a successful submission via the form", () => {
+        // cypress code
+    })
+
+    it(
+        "Should not be able to make a successful submission via the form, as all fields are required",
+        () => {
+            // cypress code
+        }
+    )
+})
