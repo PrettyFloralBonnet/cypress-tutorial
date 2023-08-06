b53792d51b3fd330a8993d61359264a11ce7fb98
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-08-06 20:36:49 +0200

    enter data into the contact form

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,12 +1,24 @@
+/// <reference types="Cypress"> />
+
 describe("Test Contact Us form on WebDriverUni", () => {
     it("Should be able to make a successful submission via the form", () => {
-        // cypress code
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#contact-us").click()
+
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("John")
+        cy.get('[name="last_name"]').type("Shepard")
+        cy.get('[name="email"]').type("john.shepard@citadel.net")
+        cy.get('textarea.feedback-input').type(
+            "I'm Commander Shepard, and this is my favorite course on Udemy."
+            )
+        cy.get('[type="submit"]').click()
     })
 
-    it(
-        "Should not be able to make a successful submission via the form, as all fields are required",
-        () => {
-            // cypress code
-        }
-    )
+    // it(
+    //     "Should not be able to make a successful submission via the form, as all fields are required",
+    //     () => {
+    //         // cypress code
+    //     }
+    // )
 })
