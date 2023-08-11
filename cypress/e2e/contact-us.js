8dc629a358f3225b809679de3a7e9160f638464e
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-08-11 21:50:54 +0200

    implement the second cypress test

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,8 +2,8 @@
 
 describe("Test Contact Us form on WebDriverUni", () => {
     it("Should be able to make a successful submission via the form", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get("#contact-us").click()
+        // cy.visit("http://www.webdriveruniversity.com")
+        // cy.get("#contact-us").click()
 
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("John")
@@ -11,14 +11,23 @@ describe("Test Contact Us form on WebDriverUni", () => {
         cy.get('[name="email"]').type("john.shepard@citadel.net")
         cy.get('textarea.feedback-input').type(
             "I'm Commander Shepard, and this is my favorite course on Udemy."
-            )
+        )
         cy.get('[type="submit"]').click()
     })
 
-    // it(
-    //     "Should not be able to make a successful submission via the form, as all fields are required",
-    //     () => {
-    //         // cypress code
-    //     }
-    // )
+    it(
+        "Should not be able to make a successful submission via the form, as all fields are required",
+        () => {
+            cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+            cy.get('[name="first_name"]').type("John")
+            cy.get('[name="last_name"]').type("Shepard")
+
+            // leaving the email field blank
+
+            cy.get('textarea.feedback-input').type(
+                "How do I shut this thing down?"
+            )
+            cy.get('[type="submit"]').click()
+        }
+    )
 })
