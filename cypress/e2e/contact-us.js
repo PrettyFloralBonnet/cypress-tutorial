7e3d8b9d0735374f849d35b303bcb076480c97ae
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-08-11 21:59:29 +0200

    add missing semicolons

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,31 +2,31 @@
 
 describe("Test Contact Us form on WebDriverUni", () => {
     it("Should be able to make a successful submission via the form", () => {
-        // cy.visit("http://www.webdriveruniversity.com")
-        // cy.get("#contact-us").click()
+        // cy.visit("http://www.webdriveruniversity.com");
+        // cy.get("#contact-us").click();
 
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
 
-        cy.get('[name="first_name"]').type("John")
-        cy.get('[name="last_name"]').type("Shepard")
-        cy.get('[name="email"]').type("john.shepard@citadel.net")
+        cy.get('[name="first_name"]').type("John");
+        cy.get('[name="last_name"]').type("Shepard");
+        cy.get('[name="email"]').type("john.shepard@citadel.net");
         cy.get('textarea.feedback-input').type(
             "I'm Commander Shepard, and this is my favorite course on Udemy."
-        )
+        );
 
-        cy.get('[type="submit"]').click()
+        cy.get('[type="submit"]').click();
     })
 
     it("Should fail to submit data via the form due to an empty email field", () => {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
 
-        cy.get('[name="first_name"]').type("John")
-        cy.get('[name="last_name"]').type("Shepard")
+        cy.get('[name="first_name"]').type("John");
+        cy.get('[name="last_name"]').type("Shepard");
 
         // leaving the email field blank
 
-        cy.get('textarea.feedback-input').type("How do I shut this thing down?")
+        cy.get('textarea.feedback-input').type("How do I shut this thing down?");
 
-        cy.get('[type="submit"]').click()
+        cy.get('[type="submit"]').click();
     })
 })
