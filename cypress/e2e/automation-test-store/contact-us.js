045c6c6b7fa8a14de97841a8f03684882b7f824b
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-08-18 21:10:10 +0200

    add another xpath selector where it replaces a possibly flaky default selector

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -4,9 +4,11 @@ describe("Test Contact Us form on Automation Test Store", () => {
     it("Should be able to make a successful submission via the form", () => {
         cy.visit("https://automationteststore.com/");
         cy.xpath("//a[contains(@href, 'contact')]").click();
+
         cy.get("#ContactUsFrm_first_name").type("John");
         cy.get("#ContactUsFrm_email").type("john.shepard@citadel.net");
         cy.get("#ContactUsFrm_enquiry").type("How do I shut this thing down?");
-        cy.get(".col-md-6 > .btn").click();
+
+        cy.xpath("//button[@title='Submit']").click();
     })
 })
