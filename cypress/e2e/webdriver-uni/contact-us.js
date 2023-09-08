b5dd1a51f9ee904022068f5d66521d163c58692a
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-09-08 23:37:24 +0200

    assert the document has a charset property with value of utf-8

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -6,6 +6,7 @@ describe("Test Contact Us form on WebDriverUni", () => {
         // cy.get("#contact-us").click();
 
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should("have.property", "charset").and("eq", "UTF-8")
 
         cy.get('[name="first_name"]').type("John");
         cy.get('[name="last_name"]').type("Shepard");
