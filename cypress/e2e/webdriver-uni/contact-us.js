9dc9ab0d4f39172e098c3f0697f3b58b2e8ac164
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2023-09-08 23:40:02 +0200

    add missing semicolon, assert the title includes the provided string

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -6,7 +6,8 @@ describe("Test Contact Us form on WebDriverUni", () => {
         // cy.get("#contact-us").click();
 
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-        cy.document().should("have.property", "charset").and("eq", "UTF-8")
+        cy.document().should("have.property", "charset").and("eq", "UTF-8");
+        cy.title().should("include", "WebDriver | Contact Us");
 
         cy.get('[name="first_name"]').type("John");
         cy.get('[name="last_name"]').type("Shepard");
